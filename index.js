e5675f4fe2b7947660be5e0fbf37eb90171abd93
Robiu-Sani <hafazrobiussani@gmail.com>
2024-07-18 10:20:13 +0600

    remove jwt

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
-const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,7 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: "http://localhost:5173", // Your frontend URL
-    credentials: true,
   })
 );
 app.use(express.json());
@@ -44,17 +42,6 @@ async function run() {
       try {
         user.password = await bcrypt.hash(user.password, saltRounds);
         const result = await users_collection.insertOne(user);
-        const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
-          expiresIn: "1h",
-        });
-
-        // Set token in a cookie
-        res.cookie("token", token, {
-          httpOnly: true, // Cookie cannot be accessed via client-side JavaScript
-          secure: false, // Send cookie only over HTTPS in production
-          sameSite: "None", // Mitigates CSRF attacks
-          maxAge: 60 * 60 * 1000, // 1 hour expiration
-        });
 
         res.json({
           message: "User created successfully",
@@ -82,22 +69,21 @@ async function run() {
           return res.status(400).send("Invalid password");
         }
 
-        const token = jwt.sign({ email }, process.env.JWT_SECRET, {
-          expiresIn: "1h",
-        });
-
-        // Set token in a cookie
-        res.cookie("token", token, {
-          httpOnly: true,
-          secure: false,
-          sameSite: "None",
-          maxAge: 60 * 60 * 1000, // 1 hour expiration
-        });
-
-        res.json({ message: "Login successful", email });
+        res.json({ message: "success", email });
       } catch (err) {
         console.error(err);
         res.status(500).send("Error logging in");
+      }
+    });
+
+    // Get users
+    app.get("/users", async (req, res) => {
+      try {
+        const result = await users_collection.find().toArray();
+        res.send(result);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send("Error fetching users");
       }
     });
 
@@ -116,5 +102,5 @@ app.get("/", (req, res) => {
 
 // Listen on port
 app.listen(port, () => {
-  console.log(`Server port is ${port}`);
+  console.log(`Server running on port ${port}`);
 });
